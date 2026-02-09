74c231c63714bc2f35761acc368a5a1541ea196c
Ken <klchen41265401@nycu.edu.tw>
2026-02-09 17:02:55 +0800

    fix: update base path to /perfume-tutorial/

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/perfume-chemistry/',
+  base: '/perfume-tutorial/',
   plugins: [vue()],
 })
